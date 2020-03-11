78461cbdc221d106dad9b52f0a36021767fa3c32
Brian Shewan <brian.shewan@nscc.ca>
2020-03-11 12:20:15 -0300

    added more dml functions

--- DML/dml.sql
+++ DML/dml.sql
@@ -59,3 +59,85 @@ WHERE CourseID = 13;
 INSERT INTO Course (CourseID, Code, Name, StudentID, EnrolDate)
 VALUES (6, @Code, @Name, @StudentID, @EnrolDate)
 SET IDENTITY_INSERT Course OFF;
+
+-- SELECT statements
+USE AdventureWorks2017;
+GO
+SELECT * FROM Person.Person
+WHERE FirstName LIKE '%Ken%';
+GO
+SELECT FirstName, LastName, JobTitle
+FROM Person.Person p
+RIGHT OUTER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID;
+GO
+SELECT FirstName, LastName, JobTitle
+FROM Person.Person p
+LEFT OUTER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID
+UNION ALL
+SELECT FirstName, LastName, JobTitle
+FROM Person.Person p
+RIGHT OUTER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID;
+GO
+SELECT DISTINCT FirstName, LastName, JobTitle
+FROM Person.Person p
+FULL OUTER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID;
+GO
+SELECT FirstName, LastName, JobTitle, COUNT(*) AS DuplicateCount
+FROM Person.Person p
+FULL OUTER JOIN HumanResources.Employee e
+ON p.BusinessEntityID = e.BusinessEntityID
+GROUP BY FirstName, LastName, JobTitle
+HAVING COUNT(*) > 1
+ORDER BY LastName;
+
+SELECT STRING_AGG (CONVERT(nvarchar(max),FirstName), ',') AS csv
+FROM Person.Person;
+
+SELECT FirstName, LastName FROM Person.Person;
+GO
+
+-- concatenating string
+SELECT FirstName + ' ' + LastName AS FullName FROM Person.Person;
+SELECT CONCAT(FirstName, ' ', LastName) AS FullName FROM Person.Person;
+-- substrings
+SELECT LEFT(FirstName, 1) AS Initial FROM Person.Person;
+SELECT FirstName, SUBSTRING(FirstName, 2, 3) FROM Person.Person;
+SELECT FirstName, CHARINDEX('a', FirstName) FROM Person.Person;
+
+SELECT
+       LOWER(CONCAT(LEFT(FirstName, 1), LEFT(LastName, 3))) AS UserName
+FROM Person.Person;
+-- trimming
+SELECT
+       LTRIM(STR(SubTotal, 10, 2)) + ' + ' +
+       LTRIM(STR(TaxAmt, 10, 2)) + ' = ' +
+       LTRIM(STR(TotalDue, 10, 2))
+FROM Sales.SalesOrderHeader;
+-- length
+SELECT (FirstName + ' ' + LastName) AS FullName,
+       LEN(FirstName + ' ' + LastName) AS Length
+FROM Person.Person;
+SELECT MAX(LEN(FirstName + ' ' + LastName)) AS MaxLength
+FROM Person.Person;
+
+-- cast/convert
+SELECT TotalDue, CAST(TotalDue AS INT) FROM Sales.SalesOrderHeader;
+SELECT TotalDue, CONVERT(INT, TotalDue) FROM Sales.SalesOrderHeader;
+
+SELECT OrderDate, CONVERT(VARCHAR, OrderDate, 127) FROM Sales.SalesOrderHeader;
+
+-- date/time functions
+SELECT OrderDate, YEAR(OrderDate), MONTH(OrderDate), DAY(OrderDate)
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2011 AND MONTH(OrderDate) = 5;
+
+SELECT OrderDate, DATEPART(HOUR, OrderDate), DATEPART(MINUTE, OrderDate)
+FROM Sales.SalesOrderHeader;
+
+
+
+
