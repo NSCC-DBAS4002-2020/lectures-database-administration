e370fc88a2b11e09ec2648b82ddd6da40384d1fa
Brian Shewan <brian.shewan@nscc.ca>
2020-04-20 08:45:40 -0300

    added more dml functions

--- DML/dml.sql
+++ DML/dml.sql
@@ -138,6 +138,84 @@ WHERE YEAR(OrderDate) = 2011 AND MONTH(OrderDate) = 5;
 SELECT OrderDate, DATEPART(HOUR, OrderDate), DATEPART(MINUTE, OrderDate)
 FROM Sales.SalesOrderHeader;
 
+DECLARE @value INT;
+SET @value = 123;
+
+SELECT IIF(@value > 100, '> 100', '<= 100') AS OneHundred;
+
+SELECT
+       FirstName, LastName,
+       IIF(CHARINDEX('a', LastName) != 0, 'Letter A in name', '') AS AinName
+FROM Person.Person;
+
+-- 1 = OK, 2 = GOOD, 3 = BAD
+SELECT CHOOSE(2, 'OK', 'GOOD', 'BAD') AS Status;
+
+-- 0 = No emails, 1 = want emails, 2 = different emails
+SELECT CHOOSE(EmailPromotion + 1, 'No Emails', 'Some Emails', 'All Emails')
+FROM Person.Person;
+
+DECLARE @letter CHAR(1);
+SET @letter = 'c';
+
+SELECT IIF(@letter = 'a', 'It is an A',
+            IIF(@letter = 'b', 'It is a B',
+                IIF(@letter = 'c', 'It is a C', 'It is NOT A, B, or C')))
+    AS LetterCheck;
+
+SELECT CASE @letter
+        WHEN 'a' THEN 'It is an A'
+        WHEN 'b' THEN 'It is a B'
+        WHEN 'c' THEN 'It is a C'
+        ELSE 'It is NOT A, B, or C'
+        END AS LetterCheck;
+
+DECLARE @letter CHAR(1);
+SET @letter = 'c';
+SELECT CASE
+        WHEN @letter = 'a' THEN 'It is an A'
+        WHEN @letter = 'b' THEN 'It is a B'
+        WHEN @letter = 'c' THEN 'It is a C'
+        ELSE 'It is NOT A, B, or C'
+        END AS LetterCheck3;
 
 
+SELECT   ProductNumber, Name, "Price Range" =
+      CASE
+         WHEN ListPrice =  0 THEN 'Mfg item - not for resale'
+         WHEN ListPrice < 50 THEN 'Under $50'
+         WHEN ListPrice >= 50 and ListPrice < 250 THEN 'Under $250'
+         WHEN ListPrice >= 250 and ListPrice < 1000 THEN 'Under $1000'
+         ELSE 'Over $1000'
+      END
+FROM Production.Product
+ORDER BY ProductNumber ;
+GO
 
+-- locates the first non-null value in a list of values
+SELECT COALESCE(NULL, NULL, NULL, 1, 2, 3, 4, 5) AS Number;
+
+SELECT COALESCE(JobTitle, FirstName, LastName) AS Title
+FROM Person.Person p
+LEFT OUTER JOIN HumanResources.Employee e ON p.BusinessEntityID = e.BusinessEntityID;
+
+-- replace JobTitle with 'Customer'
+SELECT ISNULL(JobTitle, 'Customer'), FirstName, LastName AS Title
+FROM Person.Person p
+LEFT OUTER JOIN HumanResources.Employee e ON p.BusinessEntityID = e.BusinessEntityID;
+
+SELECT JobTitle,
+       (SELECT FirstName + ' ' + LastName
+        FROM Person.Person p WHERE p.BusinessEntityID = e.BusinessEntityID)
+FROM HumanResources.Employee e;
+
+SELECT PersonType, FirstName, LastName
+FROM Person.Person;
+
+SELECT JobTitle FROM HumanResources.Employee
+WHERE BusinessEntityID IN
+    (SELECT BusinessEntityID FROM Person.Person WHERE PersonType = 'SC');
+
+SELECT FirstName, LastName FROM Person.Person
+WHERE BusinessEntityID IN
+    (SELECT CustomerID FROM Sales.Customer);
